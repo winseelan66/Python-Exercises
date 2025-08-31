9b248912ef8dc2a9b024009d7f0074d4630448da
Jeyseelan Jeybhaskaran <Jeyseelan.Jeybhaskaran@csgi.com>
2025-08-31 15:51:28 +0530

    examples of data types

---
+++ data-types/datatype-examples.py
@@ -0,0 +1,38 @@
+#Numbers
+x = 10
+y = 3.25
+z = 2+3j
+
+#Boolean
+is_python_bool = True
+
+#String
+first_name = "Jeyaseelan"
+
+#collections
+#list
+fruits = ["applie","orange","mango"]
+
+#tuple
+coordinates = (10.5,20.7)
+
+#set
+unique_numbers = {1,2,3,3}
+
+#dictionary
+person = {"name":"Jey","age":43}
+
+#NoneType
+nothing = None
+
+# Print their types
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(is_python_bool))
+print(type(first_name))
+print(type(fruits))
+print(type(coordinates))
+print(type(unique_numbers))
+print(type(person))
+print(type(nothing))
